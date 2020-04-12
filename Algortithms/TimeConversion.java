5d9fe20363e59a98c7c9e7d6ac01135fb515a0b0
akanirudh <akanirudh@hotmail.com>
2020-04-12 17:56:27 +0200

    Initial commit

---
+++ Algortithms/TimeConversion.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+class Solution {
+ 
+    private static final Scanner scan = new Scanner(System.in);
+
+    static String timeConversion(String s) {
+
+        return LocalTime.parse(s, DateTimeFormatter.ofPattern("hh:mm:ssa"))
+                        .format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+        bw.close();
+    }
+}
